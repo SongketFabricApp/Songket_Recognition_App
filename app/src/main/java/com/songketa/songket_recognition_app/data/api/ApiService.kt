304e8274527d08c71384657a73050a72136a1a25
Eka Wahyu <ekawahyu9311@gmail.com>
2023-12-06 14:21:07 +0800

    Save

--- app/src/main/java/com/songketa/songket_recognition_app/data/api/ApiService.kt
+++ app/src/main/java/com/songketa/songket_recognition_app/data/api/ApiService.kt
@@ -1,4 +1,16 @@
 package com.songketa.songket_recognition_app.data.api
 
+import com.songketa.songket_recognition_app.data.response.DatasetItem
+import okhttp3.MultipartBody
+import retrofit2.Call
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+
 interface ApiService {
+    @Multipart
+    @POST("")
+    fun uploadImage(
+        @Part file: MultipartBody.Part,
+    ): Call<DatasetItem>
 }
