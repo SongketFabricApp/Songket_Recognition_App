883abae51c294960e803ad89a2dec770ba0e89b3
Eka Wahyu <ekawahyu9311@gmail.com>
2023-12-07 19:45:13 +0800

    Save

--- app/src/main/java/com/songketa/songket_recognition_app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/songketa/songket_recognition_app/ui/home/HomeFragment.kt
@@ -6,7 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
@@ -16,16 +20,19 @@ import com.songketa.songket_recognition_app.R
 import com.songketa.songket_recognition_app.adapter.HomeMenuAdapter
 import com.songketa.songket_recognition_app.data.model.Menu
 import com.songketa.songket_recognition_app.databinding.ActivityHomeBinding
+import com.songketa.songket_recognition_app.databinding.FragmentHomeBinding
 import com.songketa.songket_recognition_app.ui.ViewModelFactory
 import com.songketa.songket_recognition_app.ui.camera.CameraActivity
 import com.songketa.songket_recognition_app.ui.camera.CameraFragment
+import com.songketa.songket_recognition_app.ui.listsongket.ListSongketActivity
+import com.songketa.songket_recognition_app.ui.maps.MapsActivity
 import com.songketa.songket_recognition_app.ui.welcome.WelcomeActivity
 import kotlinx.coroutines.launch
 
 
 class HomeFragment : Fragment() {
-    private lateinit var binding: ActivityHomeBinding
-
+    private lateinit var binding: FragmentHomeBinding
+    private val message = R.string.message
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -36,7 +43,36 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view =  inflater.inflate(R.layout.fragment_home, container, false)
+
+        val buttonMarketFinder = view.findViewById<AppCompatImageButton>(R.id.btn_market_finder)
+        val buttonScan = view.findViewById<AppCompatImageButton>(R.id.btn_scan)
+        val buttonSongketList = view.findViewById<AppCompatImageButton>(R.id.btn_songeket_list)
+        val buttonArtikel = view.findViewById<AppCompatImageButton>(R.id.btn_artikel)
+
+        buttonMarketFinder.setOnClickListener {
+            val intent = Intent(activity, MapsActivity::class.java)
+            startActivity(intent)
+        }
+        buttonScan.setOnClickListener {
+            onButtonClicked()
+        }
+        buttonSongketList.setOnClickListener {
+            val intent = Intent(activity, ListSongketActivity::class.java)
+            startActivity(intent)        }
+
+        buttonArtikel.setOnClickListener {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+        return view
+    }
+    private fun onButtonClicked() {
+        val tfFragment = CameraFragment()
+        val fragmentManager = activity?.supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager?.beginTransaction() ?: return
+        fragmentTransaction.replace(R.id.frame_layout, tfFragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
     }
 
     companion object {
